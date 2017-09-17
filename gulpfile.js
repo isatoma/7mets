114c69e0c5cfcfed026ad1458bc79948a25862d5
isatoma <isamu520@gmail.com>
2017-09-17 17:25:58 +0900

    とりあえず雛形は完成

---
+++ gulpfile.js
@@ -0,0 +1,43 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var postcss = require('gulp-postcss');
+var cssnext = require('postcss-cssnext');
+var browserSync = require('browser-sync');
+
+var paths = {
+	'scss': 'src/scss/',
+	'css': 'dist/css/'
+}
+
+gulp.task('scss', function() {
+	var processors = [
+		cssnext()
+	];
+	return gulp.src(paths.scss + '**/*.scss')
+	.pipe(sass())
+	.pipe(postcss(processors))
+	.pipe(gulp.dest(paths.css))
+});
+
+
+gulp.task('copy', function() {
+	gulp.src([
+		'src/**/*',
+	])
+	.pipe(gulp.dest('dist'));
+});
+
+
+gulp.task('watch', function(){
+	gulp.watch(paths.scss + '**/*.scss',
+		['scss']
+	);
+});
+//
+// gulp.task('browser-sync', () => {
+// 	return browserSync.init(null, {
+// 		server: './dest'
+// 	});
+// });
+//
+// gulp.task('default', ['scss', 'browser-sync', 'watch', 'copy']);
